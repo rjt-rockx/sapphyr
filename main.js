2cef5489d773f00ee05340564a8a6406908b6ab8
Nef <33611774+Neeef@users.noreply.github.com>
2018-08-11 22:14:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,28 @@ var Discord = require('discord.js');
 var client = new Discord.Client();
 var config = require("./localdata/config.json");
 var utils = require("./utils/utils.js");
+var fs = require("fs");
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+	if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+jsfile.forEach((f, i) =>{
+  let files = require(`./commands/${f}`);
+  console.log(`${f} has been loaded successfully.`);
+  if (files.help && files.help.name) {
+    bot.commands.set(files.help.name, files);
+  } else {
+    console.error(`File: ${f} does not have module.exports.help name property, therefor command cannot be loaded.`);
+         }
+    });
+});
+
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -11,6 +33,16 @@ client.on('message', msg => {
 	if (msg.content === 'ping') {
 		msg.reply('Pong!');
 	}
+  let msgArray = msg.content.split(" ");
+  let cmd = msgArray[0];
+  let args = msgArray.slice(1);
+
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(client, msg, args)
+	
 });
+
+
+
 
 client.login(config.token);
