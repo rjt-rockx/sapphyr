7f748129914a010fcd7c6886a0a9065f7992e39d
rjt <rajju3004@gmail.com>
2018-10-03 21:19:18 +0530

    Implement service handler

--- main.js
+++ main.js
@@ -1,45 +1,47 @@
-var fs = require("fs");
 var path = require("path");
 var config = require("./localdata/config.json");
 var commando = require("discord.js-commando");
-var services = require("./services");
+var { initializeServices, services } = require("./services");
 
 var client = new commando.Client({
-  owner: config.owners,
-  commandEditableDuration: 0,
-  nonCommandEditable: false,
-  unknownCommandResponse: false,
-  commandPrefix: "_"
+	owner: config.owners,
+	commandEditableDuration: 0,
+	nonCommandEditable: false,
+	unknownCommandResponse: false,
+	commandPrefix: "_"
 });
 
 
 client
-  .on("ready", () => {
-    console.log(`Logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
-  })
-  .on("commandError", (cmd, err) => {
-    if (err instanceof commando.FriendlyError) return;
-    console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
-  })
-  .on("commandBlocked", (msg, reason) => {
-    console.log(`Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ""} blocked. ${reason}`);
-  })
-  .on("commandPrefixChange", (guild, prefix) => {
-    console.log(`Prefix ${prefix === "" ? "removed" : `changed to ${prefix || "the default"}`} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
+	.on("ready", () => {
+		console.log(`Logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
+	})
+	.on("commandError", (cmd, err) => {
+		if (err instanceof commando.FriendlyError) return;
+		console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+	})
+	.on("commandBlocked", (msg, reason) => {
+		console.log(`Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ""} blocked. ${reason}`);
+	})
+	.on("commandPrefixChange", (guild, prefix) => {
+		console.log(`Prefix ${prefix === "" ? "removed" : `changed to ${prefix || "the default"}`} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
 		`);
-  })
-  .on("commandStatusChange", (guild, command, enabled) => {
-    console.log(`Command ${command.groupID}:${command.memberName} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
+	})
+	.on("commandStatusChange", (guild, command, enabled) => {
+		console.log(`Command ${command.groupID}:${command.memberName} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
 		`);
-  })
-  .on("groupStatusChange", (guild, group, enabled) => {
-    console.log(`Group ${group.id} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.`);
-  });
+	})
+	.on("groupStatusChange", (guild, group, enabled) => {
+		console.log(`Group ${group.id} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.`);
+	});
 
 client.registry
-  .registerDefaultTypes()
-  .registerDefaultGroups()
-  .registerGroup("basics")
-  .registerCommandsIn(path.join(__dirname, "commands"));
+	.registerDefaultTypes()
+	.registerDefaultGroups()
+	.registerGroup("basics")
+	.registerCommandsIn(path.join(__dirname, "commands"));
 
 client.login(config.bot.token);
+
+initializeServices(client);
+global.services = services;
--- services/index.js
+++ services/index.js
@@ -1,7 +1,67 @@
-// export all files in this folder under their filename
-require("fs").readdirSync(__dirname + "/").forEach(function (filename) {
-    if (filename.match(/\.(js|json)$/) !== null && filename !== "index.js") {
-        let name = filename.replace(new RegExp(/\.(js|json)$/, "g"), "");
-        exports[name] = require("./" + name);
-    }
+var fs = require("fs");
+
+let services = [];
+fs.readdirSync(__dirname + "/").forEach(filename => {
+	if (filename.match(/\.(js|json)$/) !== null && filename !== "index.js") {
+		let name = filename.replace(new RegExp(/\.js$/, "g"), "");
+		services.push(require("./" + name));
+	}
 });
+
+let events = [
+	"channelCreate",
+	"channelDelete",
+	"channelPinsUpdate",
+	"channelUpdate",
+	"emojiCreate",
+	"emojiDelete",
+	"emojiUpdate",
+	"guildBanAdd",
+	"guildBanRemove",
+	"guildCreate",
+	"guildDelete",
+	"guildIntegrationsUpdate",
+	"guildMemberAdd",
+	"guildMemberAvailable",
+	"guildMemberRemove",
+	"guildMemberSpeaking",
+	"guildMemberUpdate",
+	"guildUnavailable",
+	"guildUpdate",
+	"message",
+	"messageDelete",
+	"messageDeleteBulk",
+	"messageReactionAdd",
+	"messageReactionRemove",
+	"messageReactionRemoveAll",
+	"messageUpdate",
+	"presenceUpdate",
+	"rateLimit",
+	"roleCreate",
+	"roleDelete",
+	"roleUpdate",
+	"typingStart",
+	"typingStop",
+	"userUpdate",
+	"voiceStateUpdate",
+	"webhookUpdate"
+];
+
+function callServiceEvent(event, args) {
+	services.forEach(service => {
+		if (typeof service.type === "undefined") return;
+		if (typeof service.type === "string" && service.type !== "event") return;
+		if (Array.isArray(service.type) && (service.type.includes("event") === false)) return;
+		if (typeof service.on[event] !== "undefined")
+			service.on[event](...args);
+	});
+}
+
+exports.initializeServices = async client => await Promise.all(services.map(async service => {
+	if (typeof service.initialize !== "undefined")
+		await service.initialize(client);
+	for (let event of events)
+		client.on(event, (...args) => callServiceEvent(event, args));
+}));
+
+exports.services = services;
