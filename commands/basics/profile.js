a87d78f7a3d4b8f474b593d97d978e475f6a52cd
Nef <33611774+Neeef@users.noreply.github.com>
2018-10-04 17:02:19 -0400

    Update profile.js

--- commands/basics/profile.js
+++ commands/basics/profile.js
@@ -4,7 +4,7 @@ const config = require('../../localdata/config.json');
 exports.run = async (client,msg,args) => {
     let url = msg.attachments.first().url;
 
-    if(msg.author.id !== "375772663794106368") return msg.reply("You cannot do this.");
+    if(msg.author.id !== config.owners) return msg.reply("You cannot do this.");
 
     if(!url) return msg.reply("Specify an image.");
 
