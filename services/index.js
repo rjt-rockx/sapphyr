d3b0f83973ac092688766bf4f1ebdbbfa26493d4
rjt <rajju3004@gmail.com>
2018-10-07 21:41:07 +0530

    Rewrite ServiceHandler

--- services/index.js
+++ services/index.js
@@ -1,67 +1,72 @@
-var fs = require("fs");
+const fs = require("fs");
 
 let services = [];
 fs.readdirSync(__dirname + "/").forEach(filename => {
-	if (filename.match(/\.(js|json)$/) !== null && filename !== "index.js") {
+	if (filename.match(/\.js$/) !== null && filename !== "index.js") {
 		let name = filename.replace(new RegExp(/\.js$/, "g"), "");
 		services.push(require("./" + name));
 	}
 });
 
-let events = [
-	"channelCreate",
-	"channelDelete",
-	"channelPinsUpdate",
-	"channelUpdate",
-	"emojiCreate",
-	"emojiDelete",
-	"emojiUpdate",
-	"guildBanAdd",
-	"guildBanRemove",
-	"guildCreate",
-	"guildDelete",
-	"guildIntegrationsUpdate",
-	"guildMemberAdd",
-	"guildMemberAvailable",
-	"guildMemberRemove",
-	"guildMemberSpeaking",
-	"guildMemberUpdate",
-	"guildUnavailable",
-	"guildUpdate",
-	"message",
-	"messageDelete",
-	"messageDeleteBulk",
-	"messageReactionAdd",
-	"messageReactionRemove",
-	"messageReactionRemoveAll",
-	"messageUpdate",
-	"presenceUpdate",
-	"rateLimit",
-	"roleCreate",
-	"roleDelete",
-	"roleUpdate",
-	"typingStart",
-	"typingStop",
-	"userUpdate",
-	"voiceStateUpdate",
-	"webhookUpdate"
-];
+let events = {
+	channelCreate: [],
+	channelDelete: [],
+	channelPinsUpdate: [],
+	channelUpdate: [],
+	emojiCreate: [],
+	emojiDelete: [],
+	emojiUpdate: [],
+	guildBanAdd: [],
+	guildBanRemove: [],
+	guildCreate: [],
+	guildDelete: [],
+	guildIntegrationsUpdate: [],
+	guildMemberAdd: [],
+	guildMemberAvailable: [],
+	guildMemberRemove: [],
+	guildMemberSpeaking: [],
+	guildMemberUpdate: [],
+	guildUnavailable: [],
+	guildUpdate: [],
+	message: [],
+	messageDelete: [],
+	messageDeleteBulk: [],
+	messageReactionAdd: [],
+	messageReactionRemove: [],
+	messageReactionRemoveAll: [],
+	messageUpdate: [],
+	presenceUpdate: [],
+	roleCreate: [],
+	roleDelete: [],
+	roleUpdate: [],
+	typingStart: [],
+	typingStop: [],
+	userUpdate: [],
+	voiceStateUpdate: [],
+	webhookUpdate: []
+};
+let eventList = Object.keys(events);
 
-function callServiceEvent(event, args) {
-	services.forEach(service => {
-		if (typeof service.type === "undefined") return;
-		if (typeof service.type === "string" && service.type !== "event") return;
-		if (Array.isArray(service.type) && (service.type.includes("event") === false)) return;
-		if (typeof service.on[event] !== "undefined")
-			service.on[event](...args);
-	});
+function isEventBased(service) {
+	if (typeof service.type === "undefined") return false;
+	if (typeof service.type === "string" && service.type !== "event") return false;
+	if (Array.isArray(service.type) && (service.type.includes("event") === false)) return false;
+	return true;
 }
 
-exports.initializeServices = async client => await Promise.all(services.map(async service => {
-	if (typeof service.initialize !== "undefined")
-		await service.initialize(client);
-	for (let event of events)
-		client.on(event, (...args) => callServiceEvent(event, args));
-}));
+exports.initializeServices = async client => {
+	await Promise.all(services.map(async service => {
+		if (typeof service.initialize !== "undefined")
+			await service.initialize(client);
+		if (isEventBased(service) && typeof service.on === "object" && Object.keys(service.on).length > 0)
+			for (let eventName of Object.keys(service.on))
+				if (eventList.includes(eventName))
+					events[eventName].push(service.on[eventName]);
+	}));
+	for (let eventName of eventList)
+		client.on(eventName, (...args) => events[eventName].map(listener => listener(...args)));
+};
+
+exports.removeServices = () => eventList.map(eventName => events[eventName] = []);
 
 exports.services = services;
