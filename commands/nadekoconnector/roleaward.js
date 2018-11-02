91d938dc6388dd188eecd10545ff6dfcf18f6a19
Nef <33611774+Neeef@users.noreply.github.com>
2018-11-01 20:04:39 -0400

    award money to roles.

---
+++ commands/nadekoconnector/roleaward.js
@@ -0,0 +1,62 @@
+var { Command } = require('discord.js-commando');
+var { RichEmbed } = require('discord.js');
+
+module.exports = class RoleAwardCommand extends global.utils.baseCommand {
+    constructor(client) {
+        super(client, {
+            name: 'roleaward',
+            memberName: 'roleaward',
+            userPermissions: ["ADMINISTRATOR"],
+            group: 'nadekoconnector',
+            description: 'Award money to a role. Ex. <award <role> <amount> <reason>',
+            args: [
+                {
+                    key: 'Role',
+                    prompt: 'The role to award.',
+                    type: 'string'
+                },
+                {
+                    key: 'Amount',
+                    prompt: 'Amount of money to reward.',
+                    type: 'integer'
+                },
+                {
+                    key: 'Reason',
+                    prompt: 'Reason for the award useage.',
+                    type: 'string'
+                }
+            ]
+        })
+    }
+    async task(ctx) {
+        let botInfo = await ctx.nadekoConnector.getBotInfo();
+            let missingroles = new RichEmbed()
+                .setTitle("Missing Role")
+                .setDescription("Missing role to award, the role is cap sensitive.");
+            let missingamount = new RichEmbed()
+                .setTitle("Missing Amount")
+                .setDescription("Missing amount to award.");
+            let missingreason = new RichEmbed()
+                .setTitle("Missing Reason")
+                .setDescription("Missing reason to award");
+            let successEmbed = new RichEmbed()
+                .setTitle("Success")
+                .setDescription(`Successfully awarded ${ctx.args.Amount} ${botInfo.bot.currency.sign} to role ${ctx.args.Role}`);
+        // I hate embeds, feels good to get back to the actual code.
+        if (!ctx.message.guild.roles.find("name", ctx.args.Role)) return await ctx.message.channel.end(missingroles);
+        if (!ctx.args.Amount) return await ctx.message.channel.send(missingamount);
+        let responce = null;
+        if (!ctx.args.Reason) return await ctx.message.channel.send(missingreason);
+        let role = ctx.message.guild.roles.find("name", ctx.args.Role);
+        let rolemembers = role.members.map(members => members.id);
+        ctx.args.Reason = "[Sapphyr] Awarded by " + ctx.message.author + " | " + ctx.args.Reason;
+        rolemembers.forEach(m => {
+            async function addCurrency(){
+             responce = await ctx.nadekoConnector.addCurrency(m, ctx.args.Amount, ctx.args.Reason);
+             console.log("Currency added to role " + ctx.args.Role + " with reason " + ctx.args.Reason + "\n Currency added: " + ctx.args.Amount);
+            }
+            addCurrency();
+        });
+    return ctx.message.channel.send(successEmbed);
+    }
+}
