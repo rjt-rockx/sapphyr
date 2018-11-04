a8aee7b85b821c589ab27e240ac2a0da803a91f5
Nef <33611774+Neeef@users.noreply.github.com>
2018-11-04 10:37:56 -0500

    embeds & embed coloring

--- commands/nadekoconnector/award.js
+++ commands/nadekoconnector/award.js
@@ -1,4 +1,5 @@
 var { Command } = require("discord.js-commando");
+var { RichEmbed } = require('discord.js');
 module.exports = class AwardCurrencyCommand extends global.utils.baseCommand {
     constructor(client) {
         super(client, {
@@ -28,12 +29,16 @@ module.exports = class AwardCurrencyCommand extends global.utils.baseCommand {
     }
 
     async task(ctx) {
+        let noReason = new RichEmbed()
+        .setTitle("Missing Arguments")
+        .setColor("#7959ff")
+        .setDescription("Please provide a reason for the award.");
         if (!ctx.nadekoConnector)
             return await ctx.send("NadekoConnector configuration not set.");
         let botInfo = await ctx.nadekoConnector.getBotInfo();
         if (typeof botInfo.bot.currency.sign === "undefined")
             return await ctx.send("Unable to parse NadekoConnector information.");
-        if (!ctx.args.reason) return await ctx.send("No reason specified.");
+        if (!ctx.args.reason) return await ctx.send(noReason);
         let dmReason = ctx.args.reason;
         ctx.args.reason = "[Sapphyr] Awarded by: " + ctx.message.author + " | " + ctx.args.reason;
         if (ctx.args.amount === 0) return;
@@ -42,10 +47,18 @@ module.exports = class AwardCurrencyCommand extends global.utils.baseCommand {
             response = await ctx.nadekoConnector.subtractCurrency(ctx.args.user.id, ctx.args.amount, ctx.args.reason);
         if (ctx.args.amount > 0)
             response = await ctx.nadekoConnector.addCurrency(ctx.args.user.id, ctx.args.amount, ctx.args.reason);
-        if (typeof response.error !== "undefined")
-            return await ctx.send(response.message);
-        if (typeof response.error === "undefined")
-           await ctx.send(`Successfully awarded ${ctx.args.amount} ${botInfo.bot.currency.sign} to ${ctx.args.user.username}`);
-        return await ctx.args.user.send(`You've been awarded ${ctx.args.amount} ${botInfo.bot.currency.sign} by ${ctx.message.author} with reason ${dmReason}.`);
+        if (typeof response.error !== "undefined") return await ctx.send(response.message);
+        if (typeof response.error === "undefined") {
+        let Success = new RichEmbed()
+        .setTitle("Success")
+        .setColor("#7959ff")
+        .setDescription(`Successfully awarded ${ctx.args.amount} ${botInfo.bot.currency.sign} to ${ctx.args.user.username}`)
+           await ctx.send(Success);
+           let embed = new RichEmbed()
+           .setTitle("Award")
+           .setColor("#7959ff")
+           .setDescription(`You've been awarded ${ctx.args.amount} ${botInfo.bot.currency.sign} by ${ctx.message.author.tag} with reason ${dmReason}.`)
+        return await ctx.args.user.send(embed);
+        }
     }
 };
