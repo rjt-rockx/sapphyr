0516b40ba69dcad42f5048f22214c1e057061391
Nef <33611774+Neeef@users.noreply.github.com>
2018-11-04 10:52:50 -0500

    dm submitter, coloring embeds

--- commands/challenges/approve.js
+++ commands/challenges/approve.js
@@ -27,9 +27,11 @@ module.exports = class ApproveCommand extends global.utils.baseCommand {
         let rewardAmount;
             let noApprover = new RichEmbed()
                 .setTitle("Missing Approver")
+                .setColor("#7959ff")
                 .setDescription("You need to have role: `Challenge Approver` to do this.");
             let channel = new RichEmbed()
                 .setTitle("Incorrect Channel.")
+                .setColor("#7959ff")
                 .setDescription(`Please use #challenge-approval.`);
                 let role = ctx.message.guild.roles.find("name", "Challenge Approver");
         if (!ctx.message.member.roles.has(role.id)) return await ctx.send(noApprover);
@@ -49,7 +51,6 @@ module.exports = class ApproveCommand extends global.utils.baseCommand {
             if (Difficulty == "hard") {
                 rewardAmount = await ctx.db.get("challenges/hard");
             }
-            console.log(rewardAmount);
             let approved = new RichEmbed()
                 .setTitle("Challenge Approved!")
                 .addField("Challenge Approved By:", ctx.message.author)
@@ -57,8 +58,19 @@ module.exports = class ApproveCommand extends global.utils.baseCommand {
                 .addField("Submitter:", msg.author)
                 .addField("Difficulty:", ctx.args.Difficulty)
                 .addField("Money Rewarded:", `${rewardAmount} ${botInfo.bot.currency.sign}`)
+                .setColor("#7959ff")
                 .setTimestamp();
+            let approved2 = new RichEmbed()
+            .setTitle("Challenge Approved")
+            .setDescription("Your challenge has been approved.")
+            .addField("Challenge:", msg.content)
+            .addField("Submitter:", "You")
+            .addField("Difficulty:", ctx.args.Difficulty)
+            .addField("Money Rewarded:", `${rewardAmount} ${botInfo.bot.currency.sign}`)
+            .setColor("#7959ff")
+            .setTimestamp();
             msg.delete();
+            msg.author.send(approved2);
             ctx.client.channels.get("507667784285552640").send(approved);
             let reason = "[Sapphyr Challenges] " + ctx.message.author + " approved " + ctx.args.ID + " with amount: " + rewardAmount + botInfo.bot.currency.sign;
          return await ctx.nadekoConnector.addCurrency(msg.author.id, rewardAmount, reason);
