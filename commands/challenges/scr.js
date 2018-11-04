9c6a6993ace0d74664bd5a0c74d492dc20cf7c6d
rjt <rajju3004@gmail.com>
2018-11-05 00:56:51 +0530

    Commando handles missing args

--- commands/challenges/scr.js
+++ commands/challenges/scr.js
@@ -1,57 +1,31 @@
-var { Command } = require('discord.js-commando');
-var { RichEmbed } = require('discord.js');
+var { Command } = require("discord.js-commando");
+var { RichEmbed } = require("discord.js");
 
 module.exports = class SetChallengerRewardCommand extends global.utils.baseCommand {
     constructor(client) {
         super(client, {
-            name: 'setchallengerreward',
-            memberName: 'src',
+            name: "setchallengerreward",
+            memberName: "src",
             userPermissions: ["ADMINISTRATOR"],
-            group: 'challenges',
-            description: 'Set a challenge submitter reward for one of 3 difficulties.',
+            group: "challenges",
+            description: "Set a challenge submitter reward for one of 3 difficulties.",
             args: [
                 {
-                    key: 'Difficulty',
-                    prompt: 'Easy/Medium/Hard',
-                    type: 'string'
+                    key: "difficulty",
+                    prompt: "Easy/Medium/Hard",
+                    type: "string",
+                    oneOf: ["easy", "medium", "hard"]
                 },
                 {
-                    key: 'Amount',
-                    prompt: 'Amount to reward to challenge difficulty.',
-                    type: 'integer'
+                    key: "amount",
+                    prompt: "Amount to reward to challenge difficulty.",
+                    type: "integer"
                 }
             ]
-        })
+        });
     }
     async task(ctx) {
-        let error;
-        let response = null;
-        if (typeof ctx.args.Difficulty !== "string" || !ctx.args.Difficulty) {
-            error = new RichEmbed()
-            .setDescription("Please specify a string/difficulty.");
-        return await ctx.send(error);
-        }
-
-        if (!ctx.args.Amount) {
-             error = new RichEmbed()
-             .setDescription("Please specify an amount to reward to a difficulty.");
-        return await ctx.send(error);
-        }
-        let difficulty = ctx.args.Difficulty.toString();
-
-        if (difficulty == "easy") {
-            response = await ctx.db.set("challenges/easy", ctx.args.Amount);
-        return await ctx.send('Success!');
-        }
-
-        if (difficulty == "medium") {
-            response = await ctx.db.set("challenges/medium", ctx.args.Amount);
-        return await ctx.send('Success!');
-        }
-
-        if(difficulty == "hard") {
-            response = await ctx.db.set("challenges/hard", ctx.args.Amount);
-        return await ctx.send('Success!');
-        }
+        await ctx.db.set(`challenges/${ctx.args.difficulty}`, ctx.args.amount);
+        return await ctx.send("Success!");
     }
-}
+};
