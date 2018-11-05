ecaf5a9ed2900d701d981d83b1038936992cf199
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-11-04 23:36:42 -0500

    Update 8ball.js

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,35 +1,17 @@
-const { Command } = require('discord.js-commando');
-const Discord = require('discord.js');
-
+const { Command } = require("discord.js-commando"), Discord = require("discord.js");
+const choices = ["Yes.", "No.", "Maybe.", "Studies agree.", "Studies do not agree.", "Yup.", "Indeed.", "For sure.",
+  "Umm...no.", "Nope.", "Really, no, just no.", "No u.", "Nah."];
 module.exports = class EightBallCommand extends Command {
-    constructor(client){
-        super(client, {
-            name: '8ball',
-            group: 'fun',
-            memberName: '8ball',
-            description: 'Bot replies with random yes or no response in many varients.',
-            examples: ['_8ball is sapphyr awesome?']
-        })
-    }
-    async run(msg){
-        let choices = [
-            "Yes.",
-            "No.",
-            "Maybe.",
-            "Studies agree.",
-            "Studies do not agree.",
-            "Yup.",
-            "Indeed.",
-            "For sure.",
-            "Umm...no.",
-            "Nope.",
-            "Really, no, just no.",
-            "No u.",
-            "Nah."
-        ];
-        let choicesIndex = choices[Math.floor(Math.random() * choices.length)];
-        let responseEmbed = new Discord.RichEmbed()
-        .setDescription(choicesIndex);
-        msg.channel.send(responseEmbed);
-    }
-}
+  constructor(client) {
+    super(client, {
+      name: "8ball",
+      group: "fun",
+      memberName: "8ball",
+      description: "Bot replies with random yes or no response in many varients.",
+      examples: ["_8ball is sapphyr awesome?"],
+    });
+  }
+  run(msg) {
+    msg.channel.send(new Discord.RichEmbed().setDescription(choices[Math.floor(Math.random() * choices.length)]));
+  }
+};
