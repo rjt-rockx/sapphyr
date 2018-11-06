7d818571edf68016dcc20f2ff0a61f415d478cb3
rjt <rjt.rockx@gmail.com>
2018-11-06 18:51:05 +0530

    Attach mee6Api functions to context

--- utils/baseCommand.js
+++ utils/baseCommand.js
@@ -3,9 +3,12 @@ const { Command } = require("discord.js-commando");
 const datahandler = require("./datahandler.js");
 const guildDatahandler = require("./guildDatahandler.js");
 const nadekoConnector = require("./nadekoConnector.js");
+const mee6api = require("./mee6.js");
+const mee6 = new mee6api();
 const { mongoUrl } = require("../localdata/config");
 const log = require("fancy-log");
 
+mee6.startCaching();
 module.exports = class BaseCommand extends Command {
     constructor(client, commandInfo) {
         super(client, commandInfo);
@@ -27,6 +30,16 @@ module.exports = class BaseCommand extends Command {
             client: this.client
         };
         if (message.guild) context.guild = message.guild;
+        if (context.guild && context.guild.id && context.guild.member("159985870458322944")) { // if mee6 is in the guild
+            if (!mee6.checkIfExists(context.guild.id))
+                mee6.addGuild(context.guild.id);
+            context.mee6 = {
+                refreshCache: () => mee6.cacheGuilds(),
+                leaderboard: () => mee6.getCachedLeaderboard(context.guild.id),
+                roleRewards: () => mee6.getCachedRoleRewards(context.guild.id),
+                userXp: userId => mee6.getUserXp(context.guild.id, userId)
+            };
+        }
         if (typeof this.client.datahandler === "undefined") {
             this.client.datahandler = new datahandler(mongoUrl ? mongoUrl : undefined);
             await this.client.datahandler.initialize();
