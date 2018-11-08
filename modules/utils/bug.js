3acd60fe2c8ad6a66a63a380bc81d392f36df27b
rjt <rjt.rockx@gmail.com>
2018-11-08 15:25:42 +0530

    Double quotes reee

--- modules/utils/bug.js
+++ modules/utils/bug.js
@@ -1,22 +1,22 @@
-const Discord = require('discord.js');
-const { Command } = require('discord.js-commando');
+const Discord = require("discord.js");
+const { Command } = require("discord.js-commando");
 
 module.exports = class BugCommand extends global.utils.baseCommand {
     constructor(client){
         super(client, {
-            name: 'bug',
-            memberName: 'bug',
-            group: 'utils',
-            description: 'Report a bug.',
+            name: "bug",
+            memberName: "bug",
+            group: "utils",
+            description: "Report a bug.",
             examples: ["_bug desc <image attachment>"],
             args: [
                 {
-                    key: 'desc',
-                    prompt: 'Description of the issue.',
-                    type: 'string'
+                    key: "desc",
+                    prompt: "Description of the issue.",
+                    type: "string"
                 }
             ]
-        })
+        });
     }
     async task(ctx){
         if(ctx.message.attachments.size < 1) {
@@ -27,7 +27,7 @@ module.exports = class BugCommand extends global.utils.baseCommand {
             await this.client.channels.get("477644168299151375").send(bugEmbed);
             ctx.message.channel.send("Bug has been successfully reported. :thumbsup:");
             return;
-        };
+        }
         let attachment = ctx.message.attachments.first().url;
         let bEmbed = new Discord.RichEmbed()
         .setTitle("Bug Report")
@@ -37,4 +37,4 @@ module.exports = class BugCommand extends global.utils.baseCommand {
         await this.client.channels.get("477644168299151375").send(bEmbed);
         ctx.message.channel.send("Bug has been successfully reported. :thumbsup:");
     }
-}
+};
