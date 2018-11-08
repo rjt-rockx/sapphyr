04bb4642f16ed2b77b08e8000eb6ce85de25ee03
rjt <rjt.rockx@gmail.com>
2018-11-08 14:57:41 +0530

    Useful commands to explain how baseCommand works

--- utils/baseCommand.js
+++ utils/baseCommand.js
@@ -5,17 +5,19 @@ const guildDatahandler = require("./guildDatahandler.js");
 const globalDatahandler = require("./globalDatahandler.js");
 const nadekoConnector = require("./nadekoConnector.js");
 const mee6api = require("./mee6.js");
-const mee6 = new mee6api();
+const mee6 = new mee6api(undefined, 150);
 const { mongoUrl } = require("../localdata/config");
 const log = require("fancy-log");
 
-mee6.startCaching();
 module.exports = class BaseCommand extends Command {
     constructor(client, commandInfo) {
         super(client, commandInfo);
     }
 
+    // get parameters as though we're running a normal command
     async run(message, args, fromPattern) {
+
+        // parse parameters into handy contextual properties before executing a task
         let context = {
             message: message,
             msg: message,
@@ -23,17 +25,27 @@ module.exports = class BaseCommand extends Command {
             args: args,
             fromPattern: fromPattern,
             channel: message.channel,
-            user: message.member ? message.member : message.author,
-            dm: data => message.author.send(data),
-            send: data => message.channel.send(data),
+            user: message.author,
             react: message.react,
+            dm: data => message.author.send(data),
+            dmEmbed: data => message.author.send({ embed: data }),
+            send: data => message.channel.send(data),
             embed: data => message.channel.send({ embed: data }),
             client: this.client
         };
+
+        // if command is run in a guild
         if (message.guild) context.guild = message.guild;
-        if (context.guild && context.guild.id && context.guild.member("159985870458322944")) { // if mee6 is in the guild
+
+        // if mee6 is there in the guild
+        if (context.guild && context.guild.id && context.guild.member("159985870458322944")) {
             if (!mee6.checkIfExists(context.guild.id))
                 mee6.addGuild(context.guild.id);
+
+            // start caching mee6 data
+            mee6.startCaching();
+
+            // attach mee6 api commands
             context.mee6 = {
                 refreshCache: () => mee6.cacheGuilds(),
                 leaderboard: () => mee6.getCachedLeaderboard(context.guild.id),
@@ -41,22 +53,36 @@ module.exports = class BaseCommand extends Command {
                 userXp: userId => mee6.getUserXp(context.guild.id, userId)
             };
         }
+
+        // if datahandler isn't initialized
         if (typeof this.client.datahandler === "undefined") {
             this.client.datahandler = new datahandler(mongoUrl ? mongoUrl : undefined);
             await this.client.datahandler.initialize();
-            context.globalDb = new globalDatahandler(this.client.datahandler);
-            await context.globalDb.reload();
         }
+
+        // attach global db to context
+        context.globalDb = new globalDatahandler(this.client.datahandler);
+        await context.globalDb.reload();
+
+        // double checking if message is ran in a guild
         if (context.guild && context.guild.id) {
+
+            // attach guildDataHandler
             context.db = new guildDatahandler(this.client.datahandler, context.guild.id);
             await context.db.reload();
-        }
-        if (context.db) {
             let nc = await context.db.get("nadekoconnector");
+
+            // if nadekoconnector is enabled
             if (typeof nc === "object" && nc.enabled === true)
+
+                // attach nadekoConnector instance to context
                 context.nadekoConnector = new nadekoConnector(nc.address, nc.password);
         }
+
+        // execute the task with the given context
         await this.task(context);
+
+        // TODO: pre-command/post-command hooks through events
         log(`Command ${message.command.name} was executed by user ${message.author.tag} (${message.author.id}).`);
     }
 };
