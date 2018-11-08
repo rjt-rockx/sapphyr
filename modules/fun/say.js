20067ec126e7f4dc9c209b232af46fb533e94ddb
rjt <rjt.rockx@gmail.com>
2018-11-08 15:04:33 +0530

    Rewrite say to use baseCommand

--- modules/fun/say.js
+++ modules/fun/say.js
@@ -1,28 +1,25 @@
 const { Command } = require("discord.js-commando");
 const Discord = require("discord.js");
-
-module.exports = class SayCommand extends Command {
+module.exports = class SayCommand extends global.utils.baseCommand {
     constructor(client) {
         super(client, {
             name: "say",
             group: "fun",
             memberName: "say",
-            description: "bot sends args",
+            description: "Make the bot say something.",
             userPermissions: ["MANAGE_MESSAGES"],
             args: [
                 {
                     key: "text",
-                    prompt: "What text would you like the bot to say?",
+                    prompt: "The text you want the bot to say.",
                     type: "string"
                 }
             ]
         });
     }
 
-    run(msg, { text }) {
-        msg.delete();
-        let sayEmbed = new Discord.RichEmbed()
-        .setDescription(text);
-        return msg.channel.send(sayEmbed);
+    async task(ctx) {
+        await ctx.message.delete();
+        return await ctx.embed({ description: ctx.args.text });
     }
 };
