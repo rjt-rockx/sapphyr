e50732d355676f84c47a4a69097e13760efe4ebc
rjt <rjt.rockx@gmail.com>
2018-11-08 21:24:52 +0530

    globalDataHandler works owo

---
+++ modules/utils/removeGlobalData.js
@@ -0,0 +1,25 @@
+var { Command } = require("discord.js-commando");
+module.exports = class RemoveGlobalDataCommand extends global.utils.baseCommand {
+    constructor(client) {
+        super(client, {
+            name: "removeglobaldata",
+            description: "Removes stored data globally",
+            group: "utils",
+            memberName: "removeglobaldata",
+            userPermissions: ["ADMINISTRATOR"],
+            args: [
+                {
+                    key: "key",
+                    prompt: "Key that the data was stored under",
+                    type: "string",
+                    default: "data"
+                }
+            ]
+        });
+    }
+
+    async task(ctx) {
+        await ctx.globalDb.remove(ctx.args.key);
+        await ctx.message.channel.send("Data successfully removed!");
+    }
+};
