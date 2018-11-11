fa2a6a59234734babec72df63d6d56e1416496af
Nef <demskittlesdoee1888@gmail.com>
2018-11-11 14:31:33 -0500

    organization and some syntax
    sticking with smol static values until we actually get it dynamically functioning.

--- modules/challenges/approve.js
+++ modules/challenges/approve.js
@@ -23,43 +23,44 @@ module.exports = class ApproveCommand extends global.utils.baseCommand {
         });
     }
     async task(ctx) {
-        let botInfo = await ctx.nadekoConnector.getBotInfo(),
-            noApprover = new RichEmbed()
-            .setTitle("Missing Approver")
-            .setColor("#7959ff")
-            .setDescription("You need to have role: `Challenge Approver` to do this."),
-            channel = new RichEmbed()
+        let botInfo = await ctx.nadekoConnector.getBotInfo();
+            var noApprover = new RichEmbed()
+                .setTitle("Missing Approver")
+                .setColor("#7959ff")
+                .setDescription("You need to have role: `Challenge Approver` to do this.");
+        var channel = new RichEmbed()
             .setTitle("Incorrect Channel.")
             .setColor("#7959ff")
-            .setDescription("Please use #challenge-approval."),
-            role = ctx.message.guild.roles.find("name", "Challenge Approver");
+            .setDescription("Please use #challenge-approval.");
+        var role = ctx.message.guild.roles.find(role => role.name === "Challenge Approver");
         if (!ctx.message.member.roles.has(role.id)) return await ctx.send(noApprover);
-        if (!ctx.args.ID) return;
-        let appTch = ctx.client.channels.get("455252710732595211"),
-            message = await appTch.fetchMessage(ctx.args.ID),
-            rewardAmount = await ctx.db.get(`challenges/${ctx.args.difficulty}`),
-            approved = new RichEmbed()
+        if (!ctx.args.id) return;
+        let appTch = ctx.client.channels.get("455252710732595211");
+            var message = await appTch.fetchMessage(ctx.args.id);
+            var rewardAmount = await ctx.db.get(`challenges/${ctx.args.difficulty}`);
+            var approved = new RichEmbed()
             .setTitle("Challenge Approved!")
             .addField("Challenge Approved By:", ctx.message.author)
             .addField("Challenge:", message.content)
             .addField("Submitter:", message.author)
-            .addField("Difficulty:", ctx.args.Difficulty)
+            .addField("Difficulty:", ctx.args.difficulty)
             .addField("Money Rewarded:", `${rewardAmount} ${botInfo.bot.currency.sign}`)
             .setColor("#7959ff")
-            .setTimestamp(),
-            approved2 = new RichEmbed()
+            .setTimestamp();
+
+            var approved2 = new RichEmbed()
             .setTitle("Challenge Approved")
             .setDescription("Your challenge has been approved.")
             .addField("Challenge:", message.content)
             .addField("Submitter:", "You")
-            .addField("Difficulty:", ctx.args.Difficulty)
+            .addField("Difficulty:", ctx.args.difficulty)
             .addField("Money Rewarded:", `${rewardAmount} ${botInfo.bot.currency.sign}`)
             .setColor("#7959ff")
             .setTimestamp();
         await message.delete();
         await message.author.send(approved2);
         await ctx.client.channels.get("507667784285552640").send(approved);
-        let reason = "[Sapphyr Challenges] " + ctx.message.author + " approved " + ctx.args.ID + " with amount: " + rewardAmount + botInfo.bot.currency.sign;
+        let reason = "[Sapphyr Challenges] " + ctx.message.author + " approved " + ctx.args.id + " with amount: " + rewardAmount + botInfo.bot.currency.sign;
         return await ctx.nadekoConnector.addCurrency(message.author.id, rewardAmount, reason);
     }
-};
+    };
