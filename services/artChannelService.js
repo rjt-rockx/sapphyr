2a540a0d6d03ef946a0318bb67839248a2f7448c
rjt <rjt.rockx@gmail.com>
2018-11-12 17:28:01 +0530

    Revamp ArtChannel service

--- services/artChannelService.js
+++ services/artChannelService.js
@@ -1,12 +1,18 @@
+const urlRegex = require("url-regex-local")({ exact: false, strict: false });
+
 module.exports = class ArtChannelService extends global.utils.baseService {
 	constructor(client) {
 		super(client);
 	}
 
-	onMessage(ctx) {
-		if (ctx.guild.channels.find(c => c.name === "art"))
-			if (ctx.channel.id === ctx.guild.channels.find(c => c.name === "art").id && ctx.message.attachments.size > 0)
-				ctx.message.react("🔺");
+	async onMessage(ctx) {
+		let artchannels = await ctx.globalDb.get("artchannels");
+		if (!artchannels)
+			return await ctx.globalDb.set("artchannels", []);
+		let index = artchannels.map(channel => channel.id).indexOf(ctx.channel.id);
+		if (index > -1)
+			if (ctx.message.attachments.size > 0 || ctx.message.embeds.length > 0 || urlRegex.test(ctx.message.cleanContent))
+				for (let reaction of artchannels[index].reactions)
+					await ctx.message.react(reaction);
 	}
 };
-
