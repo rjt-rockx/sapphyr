65c7b46ee6e48c9527095e9371a334228161b0f0
rjt <rjt.rockx@gmail.com>
2018-11-19 03:00:53 +0530

    Was that so hard now -.-

--- modules/basics/eval.js
+++ modules/basics/eval.js
@@ -1,9 +1,8 @@
-const util = require("util"),
-    discord = require("discord.js"),
-    tags = require("common-tags"),
-    escapeRegex = require("escape-string-regexp"),
-    nl = "!!NL!!",
-    nlPattern = new RegExp(nl, "g");
+const { inspect } = require("util"),
+	{ splitMessage } = require("discord.js"),
+	{ stripIndents } = require("common-tags"),
+	escapeRegex = require("escape-string-regexp"),
+	nlPattern = new RegExp("!!NL!!", "g");
 
 module.exports = class EvalCommand extends global.utils.baseCommand {
 	constructor(client) {
@@ -22,68 +21,53 @@ module.exports = class EvalCommand extends global.utils.baseCommand {
 				}
 			]
 		});
-
-        this.lastResult = null;
+		this.lastResult = null;
 	}
 
 	async task(ctx) {
-
-        const message = ctx.message,
-            client = ctx.client,
-            objects = ctx.client.registry.evalObjects,
-            lastResult = this.lastResult;
 		const doReply = val => {
-			if(val instanceof Error) {
-				ctx.reply(`Callback error: \`${val}\``);
-			} else {
-                const result = this.makeResultMessages(val, process.hrtime(this.hrStart), null, ctx);
-				if(Array.isArray(result)) {
-					for(const item of result) ctx.send(item);
-				} else {
-					ctx.send(result);
-				}
+			if (val instanceof Error)
+				ctx.send(`Callback error: \`${val}\``);
+			else {
+				const result = this.makeResultMessages(val, process.hrtime(this.hrStart), null, ctx);
+				return Array.isArray(result) ? result.map(item => ctx.send(item)) : ctx.send(result);
 			}
-        };
-        
+		};
+		
 		let hrDiff;
 		try {
 			const hrStart = process.hrtime();
 			this.lastResult = eval(ctx.args.script);
 			hrDiff = process.hrtime(hrStart);
-		} catch(err) {
+		}
+		catch (err) {
 			return ctx.send(`Error while evaluating: \`${err}\``);
 		}
 
 		this.hrStart = process.hrtime();
 		const result = this.makeResultMessages(this.lastResult, hrDiff, ctx.args.script, ctx);
-		if(Array.isArray(result)) {
-			return result.map(item => ctx.reply(item));
-		} else {
-			return ctx.send(result);
-		}
+		return Array.isArray(result) ? result.map(item => ctx.send(item)) : ctx.send(result);
 	}
 
 	makeResultMessages(result, hrDiff, input = null, ctx) {
-		const inspected = util.inspect(result, { depth: 0 })
+		const inspected = inspect(result, { depth: 0 })
 			.replace(nlPattern, "\n")
 			.replace(this.sensitivePattern(ctx), "--snip--");
 		const split = inspected.split("\n");
-		const last = inspected.length - 1;
+		const lastInspected = inspected[inspected.length - 1];
 		const prependPart = inspected[0] !== "{" && inspected[0] !== "[" && inspected[0] !== "'" ? split[0] : inspected[0];
-		const appendPart = inspected[last] !== "}" && inspected[last] !== "]" && inspected[last] !== "'" ?
-			split[split.length - 1] :
-			inspected[last];
+		const appendPart = lastInspected !== "}" && lastInspected !== "]" && lastInspected !== "'" ? split[split.length - 1] : lastInspected;
 		const prepend = `\`\`\`javascript\n${prependPart}\n`;
 		const append = `\n${appendPart}\n\`\`\``;
-		if(input) {
-			return discord.splitMessage(tags.stripIndents`
+		if (input)
+			return splitMessage(stripIndents`
 				*Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ""}${hrDiff[1] / 1000000}ms.*
 				\`\`\`javascript
 				${inspected}
 				\`\`\`
 			`, { maxLength: 1900, prepend, append });
-		} else {
-			return discord.splitMessage(tags.stripIndents`
+		else {
+			return splitMessage(stripIndents`
 				*Callback executed after ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ""}${hrDiff[1] / 1000000}ms.*
 				\`\`\`javascript
 				${inspected}
@@ -92,13 +76,12 @@ module.exports = class EvalCommand extends global.utils.baseCommand {
 		}
 	}
 
-	sensitivePattern(ctx) {
-		if(!this._sensitivePattern) {
-			const client = ctx.client;
+	sensitivePattern({ client }) {
+		if (!this._sensitivePattern) {
 			let pattern = "";
-			if(client.token) pattern += escapeRegex(client.token);
-			Object.defineProperty(this, "_sensitivePattern", { value: new RegExp(pattern, "gi") });
+			if (client.token) pattern += escapeRegex(client.token);
+			this._sensitivePattern = new RegExp(pattern, "gi");
 		}
 		return this._sensitivePattern;
 	}
-};
+}; 
