0fd2672d522988e08c9349f044ad9483a307fed6
rjt <rjt.rockx@gmail.com>
2018-11-19 11:25:15 +0530

    Remove example in description

--- modules/challenges/approve.js
+++ modules/challenges/approve.js
@@ -6,7 +6,7 @@ module.exports = class ApproveCommand extends global.utils.baseCommand {
             name: "approve",
             memberName: "approve",
             group: "challenges",
-            description: "Approve a challenge and award the submitter. Ex. <approve <message_id> <easy/medium/hard>",
+            description: "Approve a challenge and award the submitter.",
             args: [
                 {
                     key: "id",
@@ -25,13 +25,13 @@ module.exports = class ApproveCommand extends global.utils.baseCommand {
     async task(ctx) {
         let botInfo = await ctx.nadekoConnector.getBotInfo(),
             noApprover = new RichEmbed()
-            .setTitle("Missing Approver")
-            .setColor("#7959ff")
-            .setDescription("You need to have role: `Challenge Approver` to do this."),
+                .setTitle("Missing Approver")
+                .setColor("#7959ff")
+                .setDescription("You need to have role: `Challenge Approver` to do this."),
             channel = new RichEmbed()
-            .setTitle("Incorrect Channel.")
-            .setColor("#7959ff")
-            .setDescription("Please use #challenge-approval."),
+                .setTitle("Incorrect Channel.")
+                .setColor("#7959ff")
+                .setDescription("Please use #challenge-approval."),
             role = ctx.message.guild.roles.find(role => role.name === "Challenge Approver");
         if (!ctx.message.member.roles.has(role.id)) return await ctx.send(noApprover);
         if (!ctx.args.id) return;
@@ -39,23 +39,23 @@ module.exports = class ApproveCommand extends global.utils.baseCommand {
             message = await appTch.fetchMessage(ctx.args.id),
             rewardAmount = await ctx.db.get(`challenges/${ctx.args.difficulty}`),
             approved = new RichEmbed()
-            .setTitle("Challenge Approved!")
-            .addField("Challenge Approved By:", ctx.message.author)
-            .addField("Challenge:", message.content)
-            .addField("Submitter:", message.author)
-            .addField("Difficulty:", ctx.args.difficulty)
-            .addField("Money Rewarded:", `${rewardAmount} ${botInfo.bot.currency.sign}`)
-            .setColor("#7959ff")
-            .setTimestamp(),
+                .setTitle("Challenge Approved!")
+                .addField("Challenge Approved By:", ctx.message.author)
+                .addField("Challenge:", message.content)
+                .addField("Submitter:", message.author)
+                .addField("Difficulty:", ctx.args.difficulty)
+                .addField("Money Rewarded:", `${rewardAmount} ${botInfo.bot.currency.sign}`)
+                .setColor("#7959ff")
+                .setTimestamp(),
             approved2 = new RichEmbed()
-            .setTitle("Challenge Approved")
-            .setDescription("Your challenge has been approved.")
-            .addField("Challenge:", message.content)
-            .addField("Submitter:", "You")
-            .addField("Difficulty:", ctx.args.difficulty)
-            .addField("Money Rewarded:", `${rewardAmount} ${botInfo.bot.currency.sign}`)
-            .setColor("#7959ff")
-            .setTimestamp();
+                .setTitle("Challenge Approved")
+                .setDescription("Your challenge has been approved.")
+                .addField("Challenge:", message.content)
+                .addField("Submitter:", "You")
+                .addField("Difficulty:", ctx.args.difficulty)
+                .addField("Money Rewarded:", `${rewardAmount} ${botInfo.bot.currency.sign}`)
+                .setColor("#7959ff")
+                .setTimestamp();
         await message.delete();
         await message.author.send(approved2);
         await ctx.client.channels.get("507667784285552640").send(approved);
