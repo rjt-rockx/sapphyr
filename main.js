f8368afe87896d600574cfa0c40dd5cbf2aa812e
rjt <rjt.rockx@gmail.com>
2018-11-20 20:21:33 +0530

    Cleanup some unnecessary code

--- main.js
+++ main.js
@@ -1,59 +1,56 @@
-const { resolve } = require("path"), config = require("./localdata/config.js"),
-	commando = require("discord.js-commando"), serviceHandler = require("./services"),
-	utils = require("./utils"), log = require("fancy-log");
+const { resolve } = require("path"),
+	{ owners, mongoUrl, token } = require("./localdata/config.js"),
+	{ Client: commandoClient, FriendlyError } = require("discord.js-commando"),
+	serviceHandler = require("./services"),
+	utils = require("./utils"),
+	log = require("fancy-log");
 
-let client = new commando.Client({
-	owner: config.owners,
+let client = new commandoClient({
+	owner: owners,
 	commandEditableDuration: 0,
 	nonCommandEditable: false,
 	unknownCommandResponse: false,
-	commandPrefix: "_"
+	commandPrefix: "./"
 });
 
 try {
 	client
-		.on("ready", async () => {
+		.once("ready", async () => {
 			log(`Logged in as ${client.user.tag} (${client.user.id})`);
-			client.user.setActivity("with sapphires!");
+			await client.user.setActivity("Logged in!");
+
 			// Initialize datahandler
-			client.datahandler = new utils.datahandler();
+			client.datahandler = new utils.datahandler(mongoUrl ? mongoUrl : undefined);
 			await client.datahandler.initialize();
 			log("Datahandler initialized.");
-			// Initialize utils
 			global.utils = utils;
+
 			// Initialize services
 			const services = new serviceHandler(client);
 			services.initialize(resolve("./services/"));
 			global.services = services;
 			log("Services initialized.");
-			// Register modules, commands and argument types.
+
+			// Initialize argument types
 			client.registry.registerDefaultTypes();
-			log("Default types initialized.");
+			log("Argument Types initialized.");
+
+			// Register modules
 			let { initializeModules } = require("./modules");
 			await initializeModules(client);
 			log("Modules initialized.");
+
+			// Set help activity
+			log("Ready.");
+			await client.user.setActivity(`Type ${client.commandPrefix}help for help!`);
 		})
-		.on("commandError", (cmd, err) => {
-			if (err instanceof commando.FriendlyError) return;
-			log.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
-		})
-		.on("commandBlocked", (msg, reason) => {
-			log(`Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ""} blocked. ${reason}`);
-		})
-		.on("commandPrefixChange", (guild, prefix) => {
-			log(`Prefix ${prefix === "" ? "removed" : `changed to ${prefix || "the default"}`} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
-		`);
-		})
-		.on("commandStatusChange", (guild, command, enabled) => {
-			log(`Command ${command.groupID}:${command.memberName} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
-		`);
-		})
-		.on("groupStatusChange", (guild, group, enabled) => {
-			log(`Group ${group.id} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.`);
+		.on("commandError", (command, err) => {
+			if (err instanceof FriendlyError) return;
+			log.error(`Error in command ${command.groupID}:${command.name}`, err);
 		});
+
+	client.login(token);
 }
 catch (err) {
 	log.error(err);
 }
-
-client.login(config.token);
