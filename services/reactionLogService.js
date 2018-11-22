572406b06c3152998ad7f9a302bf4a7f133b9223
rjt <rjt.rockx@gmail.com>
2018-11-22 23:36:36 +0530

    Reaction logging [WIP]

--- services/reactionLogService.js
+++ services/reactionLogService.js
@@ -7,14 +7,54 @@ module.exports = class ReactionLog extends global.utils.baseService {
         });
     }
 
-    // TODO: Store log channels in db and log guild data, similar to artChannelService
-
     async onMessageReactionAdd(ctx) {
-        console.log(`${ctx.user.tag} added reaction ${ctx.reaction.emoji.name} to ${ctx.message.id} in ${ctx.channel.name}`);
-
+        if (!ctx || !ctx.db || ctx.user.bot || !ctx.user || !ctx.reaction) return;
+        let logChannel = await ctx.db.get("reactionLogChannel");
+        if (this.client.channels.has(logChannel)) {
+            this.client.channels.get(logChannel).send({
+                embed: {
+                    author: {
+                        icon: ctx.user.displayAvatarURL,
+                        name: `${ctx.user.tag} added a reaction`
+                    },
+                    title: "Message",
+                    description: ctx.reaction.message.content,
+                    fields: [
+                        {
+                            name: "Emoji",
+                            value: ctx.reaction.emoji.toString()
+                        }
+                    ],
+                    footer: {
+                        text: `Message ID: ${ctx.reaction.message.id} | User ID: ${ctx.user.id}`
+                    }
+                }
+            });
+        }
     }
 
     async onMessageReactionRemove(ctx) {
-        console.log(`${ctx.user.tag} removed reaction ${ctx.reaction.emoji.name} from ${ctx.message.id} in ${ctx.channel.name}`);
+        if (!ctx || !ctx.db || ctx.user.bot || !ctx.user || !ctx.reaction) return;
+        let logChannel = await ctx.db.get("reactionLogChannel");
+        if (this.client.channels.has(logChannel))
+            this.client.channels.get(logChannel).send({
+                embed: {
+                    author: {
+                        icon: ctx.user.displayAvatarURL,
+                        name: `${ctx.user.tag} removed a reaction`
+                    },
+                    title: "Message",
+                    description: ctx.reaction.message.content,
+                    fields: [
+                        {
+                            name: "Emoji",
+                            value: ctx.reaction.emoji.toString()
+                        }
+                    ],
+                    footer: {
+                        text: `Message ID: ${ctx.reaction.message.id} | User ID: ${ctx.user.id}`
+                    }
+                }
+            });
     }
 };
