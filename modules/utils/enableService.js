72dc821ca2174962aa69928def1c89979b555746
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-11-22 21:57:42 -0500

    New LinTER

--- modules/utils/enableService.js
+++ modules/utils/enableService.js
@@ -1,28 +1,27 @@
 module.exports = class EnableServiceCommand extends global.utils.baseCommand {
-    constructor(client) {
-        super(client, {
-            name: "enableservice",
-            description: "Enable a service by its ID",
-            group: "utils",
-            memberName: "enableservice",
-            userPermissions: ["ADMINISTRATOR"],
-            clientPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
-            args: [
-                {
-                    key: "id",
-                    prompt: "ID of the service",
-                    type: "string",
-                }
-            ]
-        });
-    }
+  constructor(client) {
+    super(client, {
+      name: "enableservice",
+      description: "Enable a service by its ID",
+      group: "utils",
+      memberName: "enableservice",
+      userPermissions: ["ADMINISTRATOR"],
+      clientPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+      args: [
+        {
+          key: "id",
+          prompt: "ID of the service",
+          type: "string",
+        },
+      ],
+    });
+  }
 
-    async task(ctx) {
-        let serviceList = global.services.listServices().map(service => service.id);
-        let index = serviceList.map(serviceId => serviceId.toLowerCase()).indexOf(ctx.args.id.toLowerCase());
-        if (index < 0)
-            return ctx.send("Service not found.");
-        global.services.enableService(serviceList[index]);
-        return ctx.send(`Successfully enabled ${serviceList[index]}`);
-    }
+  task(ctx) {
+    let serviceList = global.services.listServices().map(service => service.id);
+    let index = serviceList.map(serviceId => serviceId.toLowerCase()).indexOf(ctx.args.id.toLowerCase());
+    if (index < 0) return ctx.send("Service not found.");
+    global.services.enableService(serviceList[index]);
+    return ctx.send(`Successfully enabled ${serviceList[index]}`);
+  }
 };
--- modules/utils/showdata.js
+++ modules/utils/showdata.js
@@ -1,28 +1,26 @@
-const { Command } = require("discord.js-commando");
 module.exports = class ShowDataCommand extends global.utils.baseCommand {
-    constructor(client) {
-        super(client, {
-            name: "showdata",
-            description: "Shows stored data",
-            group: "utils",
-            memberName: "showdata",
-            userPermissions: ["ADMINISTRATOR"],
-            clientPermissions: ["SEND_MESSAGES"],
-            args: [
-                {
-                    key: "key",
-                    prompt: "Key that the data was stored under",
-                    type: "string",
-                    default: "data"
-                }
-            ]
-        });
-    }
+  constructor(client) {
+    super(client, {
+      name: "showdata",
+      description: "Shows stored data",
+      group: "utils",
+      memberName: "showdata",
+      userPermissions: ["ADMINISTRATOR"],
+      clientPermissions: ["SEND_MESSAGES"],
+      args: [
+        {
+          key: "key",
+          prompt: "Key that the data was stored under",
+          type: "string",
+          default: "data",
+        },
+      ],
+    });
+  }
 
-    async task(ctx) {
-        let data = await ctx.db.get(ctx.args.key);
-        if (typeof data === "undefined")
-            data = "No value was set for this key!";
-        await ctx.message.channel.send(typeof data === "string" ? data : JSON.stringify(data));
-    }
+  async task(ctx) {
+    let data = await ctx.db.get(ctx.args.key);
+    if (typeof data === "undefined") data = "No value was set for this key!";
+    await ctx.message.channel.send(typeof data === "string" ? data : JSON.stringify(data));
+  }
 };
