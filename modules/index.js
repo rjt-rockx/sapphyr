446737d8aa6972aff068eba73d545a88c518b51f
rjt <rjt.rockx@gmail.com>
2018-12-01 22:09:17 +0530

    Rewrite module loader

--- modules/index.js
+++ modules/index.js
@@ -1,48 +1,41 @@
 const { lstatSync, readdirSync } = require("fs");
 const { join, parse } = require("path");
-const { checkIfExists, createIfNotExists, readJson, writeJson } = global.utils.fileManager;
+const log = require("fancy-log");
 
 const isDirectory = source => lstatSync(source).isDirectory() && !source.startsWith(".");
 const getDirectories = source => readdirSync(source).map(name => join(source, name)).filter(isDirectory).map(directory => parse(directory).name);
 const toTitleCase = str => str.replace(/\w\S*/g, txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
 
 exports.initializeModules = async function initializeModules(client) {
-	const modules = getDirectories(__dirname + "/");
-	await Promise.all(modules.map(async mdl => {
-		if (!mdl) return;
+	for (const moduleName of getDirectories(__dirname + "/")) {
+		if (!moduleName) continue;
 		try {
-			const moduleDefaults = {
-				name: toTitleCase(mdl),
-				id: mdl,
+			const defaults = {
+				name: toTitleCase(moduleName),
+				id: moduleName,
 				guarded: false,
 				description: "No description was specified for this module."
 			};
-			const indexPath = join(__dirname, mdl, "index.json");
-			if (!await checkIfExists(indexPath)) {
-				console.log(`${moduleDefaults.name} module does not have an index. Creating ...`);
-				await createIfNotExists(indexPath, moduleDefaults);
+			const index = new global.utils.file(join(__dirname, moduleName, "index.json"));
+			if (!index.exists) {
+				log(`${defaults.name} module does not have an index. Creating ...`);
+				index.write(defaults, false, true);
 			}
-			let group = await readJson(indexPath);
-			if (group.id !== moduleDefaults.id) {
-				console.log(`${moduleDefaults.name} module's index does not match. Fixing ...'`);
-				const fixedData = {
-					...moduleDefaults,
-					...group,
-					id: moduleDefaults.id,
-					description: group.description ? group.description : moduleDefaults.description
+			if (index.data.id !== defaults.id) {
+				log(`${defaults.name} module's index does not match. Fixing ...`);
+				index.data = {
+					...defaults,
+					...index.data,
+					id: defaults.id
 				};
-				await writeJson(indexPath, fixedData);
-				group = fixedData;
 			}
-			await client.registry.registerGroup(new global.utils.baseModule(client, group));
-			readdirSync(join(__dirname, mdl)).map(filename => {
-				if (filename.match(/\.js$/) !== null && filename !== "index.js") {
-					client.registry.registerCommand(require(join(__dirname, mdl, filename)));
-				}
-			});
+			await client.registry.registerGroup(new global.utils.baseModule(client, index.data));
+			for (const file of readdirSync(join(__dirname, moduleName)))
+				if (file.match(/\.js$/) !== null && file !== "index.js")
+					client.registry.registerCommand(require(join(__dirname, moduleName, file)));
 		}
 		catch (error) {
-			console.log(error);
+			log.error(error);
 		}
-	}));
+	}
 };
