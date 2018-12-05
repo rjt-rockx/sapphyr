0e1afb27c6ec6ef9d9e860f29d9e4251f4b9ef95
rjt <rjt.rockx@gmail.com>
2018-12-06 03:01:35 +0530

    [WIP] Rewrite approve command

--- modules/challenges/approve.js
+++ modules/challenges/approve.js
@@ -8,10 +8,11 @@ module.exports = class ApproveCommand extends global.utils.baseCommand {
 			group: "challenges",
 			description: "Approve a challenge and award the submitter.",
 			clientPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+			guildOnly: true,
 			args: [
 				{
 					key: "id",
-					prompt: "The ID of the message to grab.",
+					prompt: "The ID of the submission message to approve.",
 					type: "string"
 				},
 				{
@@ -24,39 +25,41 @@ module.exports = class ApproveCommand extends global.utils.baseCommand {
 		});
 	}
 	async task(ctx) {
-		const botInfo = await ctx.nadekoConnector.getBotInfo(),
-			noApprover = new RichEmbed()
-				.setTitle("Missing Approver")
-				.setColor("#7959ff")
-				.setDescription("You need to have role: `Challenge Approver` to do this."),
-			role = ctx.message.guild.roles.find(rolee => rolee.name === "Challenge Approver");
-		if (!ctx.message.member.roles.has(role.id)) return ctx.send(noApprover);
-		if (!ctx.args.id) return;
-		const appTch = this.client.channels.get("455252710732595211"),
-			message = await appTch.fetchMessage(ctx.args.id),
-			rewardAmount = await ctx.db.get(`challenges/${ctx.args.difficulty}`),
-			approved = new RichEmbed()
-				.setTitle("Challenge Approved!")
-				.addField("Challenge Approved By:", ctx.message.author)
-				.addField("Challenge:", message.content)
-				.addField("Submitter:", message.author)
-				.addField("Difficulty:", ctx.args.difficulty)
-				.addField("Money Rewarded:", `${rewardAmount} ${botInfo.bot.currency.sign}`)
-				.setColor("#7959ff")
-				.setTimestamp(),
-			approved2 = new RichEmbed()
-				.setTitle("Challenge Approved")
-				.setDescription("Your challenge has been approved.")
-				.addField("Challenge:", message.content)
-				.addField("Submitter:", "You")
-				.addField("Difficulty:", ctx.args.difficulty)
-				.addField("Money Rewarded:", `${rewardAmount} ${botInfo.bot.currency.sign}`)
-				.setColor("#7959ff")
-				.setTimestamp();
-		await message.delete();
-		await message.author.send(approved2);
-		await this.client.channels.get("507667784285552640").send(approved);
-		const reason = `[Sapphyr Challenges] ${ctx.message.author} approved ${ctx.args.id} with amount: ${rewardAmount}${botInfo.bot.currency.sign}`;
-		return ctx.nadekoConnector.addCurrency(message.author.id, rewardAmount, reason);
+
+		// validity checks
+		const { approverRole, approverChannel } = await ctx.db.get();
+		if (!approverRole)
+			return ctx.selfDestruct(`Approver role not specified. Please specify an approver role using ${ctx.prefix}approverRole`);
+		if (!approverChannel)
+			return ctx.selfDestruct(`Approver channel not specified. Please specify an approver channel using ${ctx.prefix}approverChannel`);
+		if (!ctx.guild.roles.has(approverRole)) {
+			await ctx.db.set("approverRole", "");
+			return ctx.selfDestruct("Approver role not found.");
+		}
+		if (!ctx.guild.channels.has(approverChannel)) {
+			await ctx.db.set("approverChannel", "");
+			return ctx.selfDestruct("Approver channel not found.");
+		}
+		if (ctx.channel.id !== approverChannel)
+			return ctx.selfDestruct(`This command can only be used in #${ctx.guild.channels.get(approverChannel).name}`);
+		if (!ctx.member.roles.has(approverRole))
+			return ctx.selfDestruct(`You need the ${ctx.guild.roles.get(approverRole).name} to use this command.`);
+
+		const challengeData = await ctx.db.get("challengeData") || await ctx.db.set("challengeData", {});
+		let submission;
+		try {
+			submission = await ctx.channel.fetchMessage(ctx.args.id);
+		}
+		catch (error) {
+			return ctx.selfDestruct("Unable to fetch the message. Make sure the message exists in this channel.");
+		}
+		if (!challengeData[submission.author.id])
+			challengeData[submission.author.id] = [];
+		if (!challengeData.rewards)
+			return ctx.selfDestruct("No challenge rewards specified for this guild.");
+		if (!ctx.nadekoConnector)
+			return ctx.selfDestruct("No NadekoConnector configuration found for this guild.");
+
+		ctx.send("wooot all checks passed");
 	}
 };
