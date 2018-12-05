4a2e1d789f5f16cedc980eb289d0412e62d09b08
rjt <rjt.rockx@gmail.com>
2018-12-06 01:16:35 +0530

    Add ctx.selfDestruct

--- utils/baseCommand.js
+++ utils/baseCommand.js
@@ -1,6 +1,8 @@
 const { Command } = require("discord.js-commando"),
-	datahandler = require("./datahandler.js"), guildDatahandler = require("./guildDatahandler.js"),
-	globalDatahandler = require("./globalDatahandler.js"), nadekoConnector = require("./nadekoConnector.js"),
+	datahandler = require("./datahandler.js"),
+	guildDatahandler = require("./guildDatahandler.js"),
+	globalDatahandler = require("./globalDatahandler.js"),
+	nadekoConnector = require("./nadekoConnector.js"),
 	{ mongoUrl } = require("../localdata/config"),
 	log = require("fancy-log");
 
@@ -21,10 +23,13 @@ module.exports = class BaseCommand extends Command {
 			dm: message.author.send,
 			dmEmbed: data => message.author.send({ embed: data }),
 			send: message.channel.send,
-			embed: data => message.channel.send({ embed: data })
+			embed: data => message.channel.send({ embed: data }),
+			selfDestruct: (data, seconds = 5) => message.channel.send(data).then(msg => msg.delete(seconds * 1000))
 		};
 		if (message.guild)
 			context.guild = message.guild;
+		if (message.member)
+			context.member = message.member;
 		if (typeof this.client.datahandler === "undefined") {
 			this.client.datahandler = new datahandler(mongoUrl ? mongoUrl : undefined);
 			await this.client.datahandler.initialize();
