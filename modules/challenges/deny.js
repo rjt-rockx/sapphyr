05ae5223bc0db637e37b53dd523a261d074399d6
rjt <rjt.rockx@gmail.com>
2018-12-10 20:44:25 +0530

    Update deny command to match approve command

--- modules/challenges/deny.js
+++ modules/challenges/deny.js
@@ -17,31 +17,39 @@ module.exports = class DenyCommand extends global.utils.baseCommand {
 		});
 	}
 	async task(ctx) {
-		const noApprover = new RichEmbed()
-			.setTitle("Missing Approver")
-			.setColor("#7959ff")
-			.setDescription("You need to have role: `Challenge Approver` to do this."),
-			role = ctx.message.guild.roles.find("name", "Challenge Approver");
-		if (!ctx.message.member.roles.has(role.id))
-			return ctx.send(noApprover);
-		if (!ctx.args.id) return;
-		const appTch = this.client.channels.get("455252710732595211");
-		const message = await appTch.fetchMessage(ctx.args.id),
-			embed = new RichEmbed()
-				.setTitle("Denied.")
-				.setColor("#7959ff")
-				.setDescription("Your challenge submission has been denied.");
-		await message.delete();
-		await message.author.send(embed);
-		const success = new RichEmbed()
-			.setTitle("Success.")
-			.setDescription("Challenge Denied")
-			.setColor("#7959ff")
-			.addField("Denied by:", ctx.message.author)
-			.addField("Submitter:", message.author)
-			.addField("Challenge:", message.content)
-			.addField("Money Rewarded:", "False")
-			.setTimestamp();
-		return ctx.send(success);
+		const { approverRole, approverChannel } = await ctx.db.get();
+		if (!approverRole)
+			return ctx.send(`Approver role not specified. Please specify an approver role using ${ctx.prefix}approverRole`);
+		if (!approverChannel)
+			return ctx.send(`Approver channel not specified. Please specify an approver channel using ${ctx.prefix}approverChannel`);
+		if (!ctx.guild.roles.has(approverRole)) {
+			await ctx.db.set("approverRole", "");
+			return ctx.send("Approver role not found.");
+		}
+		if (!ctx.guild.channels.has(approverChannel)) {
+			await ctx.db.set("approverChannel", "");
+			return ctx.send("Approver channel not found.");
+		}
+		if (ctx.channel.id !== approverChannel)
+			return ctx.send(`This command can only be used in #${ctx.guild.channels.get(approverChannel).name}`);
+		if (!ctx.member.roles.has(approverRole))
+			return ctx.send(`You need the ${ctx.guild.roles.get(approverRole).name} to use this command.`);
+
+		const challengeData = await ctx.db.get("challengeData") || await ctx.db.set("challengeData", {});
+		let submission;
+		try {
+			submission = await ctx.channel.fetchMessage(ctx.args.messageId);
+		}
+		catch (error) {
+			return ctx.send("Unable to fetch the message. Make sure the message exists in this channel.");
+		}
+		if (!challengeData[submission.author.id])
+			challengeData[submission.author.id] = [];
+		if (!challengeData.rewards)
+			return ctx.send("No challenge rewards specified for this guild.");
+		if (!ctx.nadekoConnector)
+			return ctx.send("No NadekoConnector configuration found for this guild.");
+
+		ctx.send("wooot all checks passed");
 	}
 };
