4c29ab7c987157b59c02233626a1620359647da6
rjt <rjt.rockx@gmail.com>
2018-12-10 20:44:10 +0530

    Use ctx.send instead of ctx.selfDestruct for now

--- modules/challenges/approve.js
+++ modules/challenges/approve.js
@@ -23,40 +23,38 @@ module.exports = class ApproveCommand extends global.utils.baseCommand {
 		});
 	}
 	async task(ctx) {
-
-		// validity checks
 		const { approverRole, approverChannel } = await ctx.db.get();
 		if (!approverRole)
-			return ctx.selfDestruct(`Approver role not specified. Please specify an approver role using ${ctx.prefix}approverRole`);
+			return ctx.send(`Approver role not specified. Please specify an approver role using ${ctx.prefix}approverRole`);
 		if (!approverChannel)
-			return ctx.selfDestruct(`Approver channel not specified. Please specify an approver channel using ${ctx.prefix}approverChannel`);
+			return ctx.send(`Approver channel not specified. Please specify an approver channel using ${ctx.prefix}approverChannel`);
 		if (!ctx.guild.roles.has(approverRole)) {
 			await ctx.db.set("approverRole", "");
-			return ctx.selfDestruct("Approver role not found.");
+			return ctx.send("Approver role not found.");
 		}
 		if (!ctx.guild.channels.has(approverChannel)) {
 			await ctx.db.set("approverChannel", "");
-			return ctx.selfDestruct("Approver channel not found.");
+			return ctx.send("Approver channel not found.");
 		}
 		if (ctx.channel.id !== approverChannel)
-			return ctx.selfDestruct(`This command can only be used in #${ctx.guild.channels.get(approverChannel).name}`);
+			return ctx.send(`This command can only be used in #${ctx.guild.channels.get(approverChannel).name}`);
 		if (!ctx.member.roles.has(approverRole))
-			return ctx.selfDestruct(`You need the ${ctx.guild.roles.get(approverRole).name} to use this command.`);
+			return ctx.send(`You need the ${ctx.guild.roles.get(approverRole).name} to use this command.`);
 
 		const challengeData = await ctx.db.get("challengeData") || await ctx.db.set("challengeData", {});
 		let submission;
 		try {
-			submission = await ctx.channel.fetchMessage(ctx.args.id);
+			submission = await ctx.channel.fetchMessage(ctx.args.messageId);
 		}
 		catch (error) {
-			return ctx.selfDestruct("Unable to fetch the message. Make sure the message exists in this channel.");
+			return ctx.send("Unable to fetch the message. Make sure the message exists in this channel.");
 		}
 		if (!challengeData[submission.author.id])
 			challengeData[submission.author.id] = [];
 		if (!challengeData.rewards)
-			return ctx.selfDestruct("No challenge rewards specified for this guild.");
+			return ctx.send("No challenge rewards specified for this guild.");
 		if (!ctx.nadekoConnector)
-			return ctx.selfDestruct("No NadekoConnector configuration found for this guild.");
+			return ctx.send("No NadekoConnector configuration found for this guild.");
 
 		ctx.send("wooot all checks passed");
 	}
