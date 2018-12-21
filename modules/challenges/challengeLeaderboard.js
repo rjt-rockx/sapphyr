b3b8b0b3033571e950b222b32426ac4b7581917b
rjt <rjt.rockx@gmail.com>
2018-12-22 03:58:03 +0530

    Command to display challenge leaderboard

---
+++ modules/challenges/challengeLeaderboard.js
@@ -0,0 +1,65 @@
+const sum = arr => arr.length && arr.length === 0 ? 0 : arr.reduce((sum, value) => (typeof sum === "number" ? sum : 0) + value);
+
+module.exports = class ChallengeLeaderboardCommand extends global.utils.baseCommand {
+	constructor(client) {
+		super(client, {
+			name: "challengeleaderboard",
+			memberName: "challengeleaderboard",
+			aliases: ["chlb"],
+			group: "challenges",
+			description: "See this guild's challenge leaderboard",
+			clientPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+			guildOnly: true,
+			args: [
+				{
+					key: "sortBy",
+					prompt: "Method to sort the challenge leaderboard by",
+					type: "string",
+					oneOf: ["count", "reward"],
+					default: "count"
+				}
+			]
+		});
+	}
+
+	async task(ctx) {
+		const challengeData = await ctx.db.get("challengeData") || await ctx.db.set("challengeData", {});
+		if (typeof challengeData.users !== "object") {
+			challengeData.users = {};
+			await ctx.db.set("challengeData", challengeData);
+		}
+		const result = await ctx.nadekoConnector.getBotInfo();
+		if (result.error) {
+			console.log(`[Error] NadekoConnector: ${result.message}`);
+			return ctx.send("Unable to get bot information.");
+		}
+		const sign = result.bot.currency.sign;
+		const leaderboard = this.getLeaderboard(challengeData, ctx.args.sortBy);
+		if (!leaderboard || (Array.isArray(leaderboard) && leaderboard.length < 1))
+			return ctx.send("Unable to fetch leaderboard.");
+		const leaderboardFields = leaderboard.map(user => ({
+			name: `#${user.rank} - ${this.client.users.get(user.userId).tag}`,
+			value: `${user.count} challenges completed.\n${user.reward} ${sign} earned.`
+		}));
+		return new global.utils.fieldPaginator(ctx.channel, ctx.user, leaderboardFields, 15, {
+			embedTemplate: { title: `Challenge leaderboard for ${ctx.guild.name}` },
+			chunkSize: 7
+		});
+	}
+
+	getLeaderboard(challengeData, sortBy = "count") {
+		let leaderboard = [];
+		for (const [userId, challengeHistory] of Object.entries(challengeData.users)) {
+			leaderboard.push({
+				userId,
+				count: challengeHistory.length,
+				reward: sum(challengeHistory.map(entry => entry.challenge.reward))
+			});
+		}
+		if (sortBy === "reward")
+			leaderboard = leaderboard.sort((a, b) => b.reward - a.reward);
+		else
+			leaderboard = leaderboard.sort((a, b) => b.count - a.count);
+		return leaderboard.map((element, index) => ({ ...element, rank: index + 1 }));
+	}
+};
