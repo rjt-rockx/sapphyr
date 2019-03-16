e7b5af7dcb42346131f45f81eef41a4455cfe457
rjt <rjt.rockx@gmail.com>
2019-03-17 03:38:04 +0530

    Cleanup awarduser

--- modules/nadekoconnector/awardUser.js
+++ modules/nadekoconnector/awardUser.js
@@ -1,4 +1,3 @@
-const { RichEmbed } = require("discord.js");
 module.exports = class AwardUserCommand extends global.utils.baseCommand {
 	constructor(client) {
 		super(client, {
@@ -29,27 +28,22 @@ module.exports = class AwardUserCommand extends global.utils.baseCommand {
 	}
 
 	async task(ctx) {
-		const noReason = new RichEmbed().setTitle("Missing Arguments").setColor("#7959ff")
-			.setDescription("Please provide a reason for the award.");
-
-		if (!ctx.nadekoConnector) return ctx.send("NadekoConnector configuration not set.");
+		if (!ctx.nadekoConnector)
+			return ctx.send("NadekoConnector configuration not set.");
 		const botInfo = await ctx.nadekoConnector.getBotInfo();
-		if (typeof botInfo.bot.currency.sign === "undefined") return ctx.send("Unable to parse NadekoConnector information.");
-		if (!ctx.args.reason) return ctx.send(noReason);
-		const dmReason = ctx.args.reason;
+		if (typeof botInfo.bot.currency.sign === "undefined")
+			return ctx.send("Unable to parse NadekoConnector information.");
+		if (!ctx.args.reason)
+			return ctx.send("No reason specified.");
 		ctx.args.reason = `[Sapphyr] Awarded by: ${ctx.message.author} | ${ctx.args.reason}`;
-		if (ctx.args.amount === 0) return;
-		let response = null;
-		if (ctx.args.amount < 0) response = await ctx.nadekoConnector.subtractCurrency(ctx.args.user.id, ctx.args.amount, ctx.args.reason);
-		if (ctx.args.amount > 0) response = await ctx.nadekoConnector.addCurrency(ctx.args.user.id, ctx.args.amount, ctx.args.reason);
-		if (typeof response.error !== "undefined") return ctx.send(response.message);
-		if (typeof response.error === "undefined") {
-			const Success = new RichEmbed().setTitle("Success").setColor("#7959ff")
-				.setDescription(`Successfully awarded ${ctx.args.amount} ${botInfo.bot.currency.sign} to ${ctx.args.user.username}`);
-			await ctx.send(Success);
-			const embed = new RichEmbed().setTitle("Award").setColor("#7959ff")
-				.setDescription(`You've been awarded ${ctx.args.amount} ${botInfo.bot.currency.sign} by ${ctx.message.author.tag} with reason ${dmReason}.`);
-			return ctx.args.user.send(embed);
+		if (!ctx.args.amount)
+			return ctx.send("Invalid amount specified.");
+		const response = await ctx.nadekoConnector[`${ctx.args.amount > 0 ? "add" : "subtract"}Currency`](ctx.args.user.id, ctx.args.amount, ctx.args.reason);
+		if (response.error)
+			return ctx.send(response.message);
+		if (!response.error) {
+			await ctx.send(`Successfully awarded ${ctx.args.amount} ${botInfo.bot.currency.sign} to ${ctx.args.user.tag}`);
+			return ctx.dm(`You've been awarded ${ctx.args.amount} ${botInfo.bot.currency.sign} by ${ctx.user.tag}!`);
 		}
 	}
 };
