e7198f31796fc5797bd7037921569d409b189e08
rjt <rjt.rockx@gmail.com>
2019-03-30 00:58:45 +0530

    Cleanup Cash code

--- modules/nadekoconnector/cash.js
+++ modules/nadekoconnector/cash.js
@@ -1,4 +1,3 @@
-const { RichEmbed } = require("discord.js");
 module.exports = class CashCommand extends global.utils.baseCommand {
 	constructor(client) {
 		super(client, {
@@ -21,13 +20,11 @@ module.exports = class CashCommand extends global.utils.baseCommand {
 
 	async task(ctx) {
 		if (!ctx.nadekoConnector) return ctx.send("NadekoConnector configuration not set.");
-		const botInfo = await ctx.nadekoConnector.getBotInfo();
-		if (typeof botInfo.bot.currency.sign === "undefined") return ctx.send("Unable to parse NadekoConnector information.");
+		const result = await ctx.nadekoConnector.getBotInfo();
+		if (result.error || !result.bot.currency.sign)
+			return ctx.send("Unable to parse NadekoConnector information.");
 		const targetUser = ctx.args.user === "self" ? ctx.message.author : ctx.args.user,
-			currency = await ctx.nadekoConnector.getCurrency(targetUser.id),
-			embed = new RichEmbed()
-				.setColor("#7959ff")
-				.setDescription(`**${targetUser.tag}** has ${currency.currency} ${botInfo.bot.currency.sign}`);
-		return ctx.send(embed);
+			currency = await ctx.nadekoConnector.getCurrency(targetUser.id);
+		return ctx.embed({ description: `**${targetUser.tag}** has ${currency.currency} ${result.bot.currency.sign}` });
 	}
 };
