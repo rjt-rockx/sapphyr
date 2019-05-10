7afbc9a5b518eac83b03433a3e1262bb2350fbd9
rjt <rjt.rockx@gmail.com>
2019-05-10 14:57:30 +0530

    Add commands to persist/stack challenge roles

---
+++ modules/challenges/persistChallengeRoles.js
@@ -0,0 +1,36 @@
+module.exports = class PersistChallengeRolesCommand extends global.utils.baseCommand {
+	constructor(client) {
+		super(client, {
+			name: "persistchallengeroles",
+			description: "Make challenge roles persistent i.e. challenge roles will be re-added every 15 minutes if manually removed.",
+			group: "challenges",
+			memberName: "persistchallengeroles",
+			aliases: ["prstchrls"],
+			userPermissions: ["ADMINISTRATOR"],
+			clientPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+			args: [
+				{
+					key: "action",
+					prompt: "Whether to enable or disable persisting challenge roles.",
+					type: "string",
+					oneOf: ["enable", "disable"]
+				}
+			]
+		});
+	}
+
+	async task(ctx) {
+		const challengeRoles = await ctx.db.get("challengeRoles") || await ctx.db.set("challengeRoles", {});
+		if (ctx.args.action.toLowerCase() === "enable") {
+			challengeRoles.persistent = true;
+			await ctx.db.set("challengeRoles", challengeRoles);
+			return ctx.send("Challenge roles will persist from now on.");
+		}
+		else if (ctx.args.action.toLowerCase() === "disable") {
+			challengeRoles.persistent = false;
+			await ctx.db.set("challengeRoles", challengeRoles);
+			return ctx.send("Challenge roles will not persist from now on.");
+		}
+		else return ctx.send("Invalid action specified.");
+	}
+};
---
+++ modules/challenges/stackChallengeRoles.js
@@ -0,0 +1,36 @@
+module.exports = class StackChallengeRolesCommand extends global.utils.baseCommand {
+	constructor(client) {
+		super(client, {
+			name: "stackchallengeroles",
+			description: "Stack challenge roles over the previous ones.",
+			group: "challenges",
+			memberName: "stackchallengeroles",
+			aliases: ["stkchrls"],
+			userPermissions: ["ADMINISTRATOR"],
+			clientPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+			args: [
+				{
+					key: "action",
+					prompt: "Whether to enable or disable stacking challenge roles.",
+					type: "string",
+					oneOf: ["enable", "disable"]
+				}
+			]
+		});
+	}
+
+	async task(ctx) {
+		const challengeRoles = await ctx.db.get("challengeRoles") || await ctx.db.set("challengeRoles", {});
+		if (ctx.args.action.toLowerCase() === "enable") {
+			challengeRoles.stacked = true;
+			await ctx.db.set("challengeRoles", challengeRoles);
+			return ctx.send("Challenge roles will stack from now on.");
+		}
+		else if (ctx.args.action.toLowerCase() === "disable") {
+			challengeRoles.stacked = false;
+			await ctx.db.set("challengeRoles", challengeRoles);
+			return ctx.send("Challenge roles will not stack from now on.");
+		}
+		else return ctx.send("Invalid action specified.");
+	}
+};
